e2423d949cf525d6e27dbff2c6b80ec770ac2d1d
hrn42 <hrn42@220.86.74.92>
2020-11-27 00:34:55 +0900

    20201127 전혀 모르겠음 학원가서 생각해보기로 함...

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -4,9 +4,19 @@ public class Account {
 
     private String accountNo;
     private int balance;
+    public int menuNo;
     
     //생성자 작성
-    
-    //필요한 메소드 작성
 
+	public Account(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	
+    //필요한 메소드 작성
+	
+	public void deposit(int balance) {
+		if(menuNo == 1) {
+			int result = result + money;
+		}
+	}
 }
