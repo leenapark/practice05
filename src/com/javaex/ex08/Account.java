bb75afbcfd1c7432d41a93c4818e1e9c459e465f
'leena park' <'hrn444@naver.com'>
2020-11-27 11:24:59 +0900

    20201127 8번 문제 완성

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -3,20 +3,29 @@ package com.javaex.ex08;
 public class Account {
 
     private String accountNo;
-    private int balance;
-    public int menuNo;
+    private static int balance;
+    public static int money;
     
     //생성자 작성
-
-	public Account(String accountNo) {
-		this.accountNo = accountNo;
-	}
-	
+    public Account(String accountNo) {
+    	this.accountNo = accountNo;
+    }
+    
     //필요한 메소드 작성
-	
-	public void deposit(int balance) {
-		if(menuNo == 1) {
-			int result = result + money;
-		}
-	}
+    
+    public static void deposit(int balance) {
+    	money = money + balance;
+    }
+    
+    public static void withdraw(int balance) {
+    	money = money - balance;
+ 
+    }
+
+    
+    public static int showBalance() {
+    	System.out.println(money);
+    	return money;
+    }
+    
 }
